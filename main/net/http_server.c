2883dfd78eaeb27c9f8570485dcfcf8de8759004
ghosoft <zhaitao.as@outlook.com>
2024-10-25 15:27:14 +0800

    fix a warrning

--- main/net/http_server.c
+++ main/net/http_server.c
@@ -198,140 +198,7 @@ void free_psram_buffer(psram_buffer_t *buffer)
 }
 
 // 上传文件处理程序，存储到 PSRAM
-esp_err_t upload_post_handler(httpd_req_t *req)
-{
-	char buf[BUFFER_SIZE];
-	int received;
-	size_t remaining_size = MAX_FILE_SIZE;
-
-	ESP_LOGI(TAG, "httpd_req_t *req->method= %d", req->method);
-	ESP_LOGI(TAG, "httpd_req_t *req.uri= %s", req->uri);
-	ESP_LOGI(TAG, "httpd_req_t *req.content_len= %d", req->content_len);
-
-	// 初始化 PSRAM 缓存
-	psram_buffer_t *psram_buf = init_psram_buffer(MAX_FILE_SIZE);
-	if (!psram_buf) {
-		return ESP_FAIL;
-	}
-
-	ESP_LOGI(TAG, "Receiving file and storing to PSRAM...");
-
-	// 循环接收并存储到 PSRAM
-	while ((received = httpd_req_recv(req, buf, sizeof(buf))) > 0) {
-		// 检查剩余空间是否足够
-		if (psram_buf->offset + received > psram_buf->size) {
-			ESP_LOGE(TAG,
-				 "Not enough PSRAM space to store the file");
-			free_psram_buffer(psram_buf);
-			return ESP_FAIL;
-		}
-
-		// 将接收到的数据拷贝到 PSRAM 中
-		memcpy(psram_buf->data + psram_buf->offset, buf, received);
-		psram_buf->offset += received;
-		remaining_size -= received;
-
-		printf(".");
-		fflush(stdout); // 手动刷新缓冲区
-	}
-	printf("\r\n");
-
-	if (received < 0) {
-		ESP_LOGE(TAG, "File upload failed");
-		free_psram_buffer(psram_buf);
-		return ESP_FAIL;
-	}
-
-	// find file start
-	const unsigned char *start_string = (unsigned char *)"\x0d\x0a\x0d\x0a";
-	const void *pos = memmem(psram_buf->data, 1024, start_string, 4);
-	if (pos == NULL) {
-		ESP_LOGE(TAG, "File received has fault");
-	}
-	ptrdiff_t offset_file_start = (const unsigned char *)pos -
-				      (const unsigned char *)(psram_buf->data);
-	offset_file_start += 4;
-	ESP_LOGI(TAG, "File offset = %d", (int)offset_file_start);
-
-	// find file ends
-	const char *end_string = "------WebKitFormBoundary";
-	size_t search_size = req->content_len > 1024 ?
-				     1024 :
-				     req->content_len; // Adjust search size
-
-	pos = memmem(psram_buf->data + req->content_len - search_size,
-		     search_size, end_string, strlen(end_string));
-	ptrdiff_t offset_file_end = (const unsigned char *)pos -
-				    (const unsigned char *)(psram_buf->data);
-	offset_file_end -= 2;
-	ESP_LOGI(TAG, "File offset = %d", (int)offset_file_end);
-
-	if ((offset_file_end < offset_file_start) || (offset_file_start < 0)) {
-		ESP_LOGI(TAG, "File offset file fail");
-		// Send success response in JSON format
-		httpd_resp_set_type(req, "application/json");
-		const char *resp_str =
-			"{\"code\":500, \"msg\":\"Upload data parse fail.\"}";
-		httpd_resp_send(req, resp_str, strlen(resp_str));
-
-		return ESP_FAIL;
-	}
-
-	ESP_LOGI(TAG, "File upload successful, total size: %zu bytes",
-		 psram_buf->offset);
-
-	// Send success response in JSON format
-	httpd_resp_set_type(req, "application/json");
-	const char *resp_str = "{\"code\":200, \"msg\":\"Upload complete.\"}";
-	httpd_resp_send(req, resp_str, strlen(resp_str));
-
-	// 创建并打开文件
-	FILE *f = fopen(SDCARD_MOUNT_POINT "/request.bin", "wb+");
-	if (f == NULL) {
-		ESP_LOGE("SDMMC", "Failed to open file for writing");
-		sdcard_unmount();
-		return ESP_FAIL;
-	}
-
-	// 写入内容到文件
-	fwrite(psram_buf->data, sizeof(char), psram_buf->offset, f);
-	fclose(f);
-
-	f = fopen(SDCARD_MOUNT_POINT "/upload.jpg", "wb+");
-	if (f == NULL) {
-		ESP_LOGE("SDMMC", "Failed to open file for writing");
-		sdcard_unmount();
-		return ESP_FAIL;
-	}
-
-	// 写入内容到文件
-	fwrite(psram_buf->data + offset_file_start, sizeof(char),
-	       offset_file_end - offset_file_start, f);
-	fclose(f);
-
-	// TODO: 进度条
-
-	// 释放 PSRAM 缓存
-	free_psram_buffer(psram_buf);
-
-	int64_t start_time = esp_timer_get_time();
-	display_jpg_file(SDCARD_MOUNT_POINT "/upload.jpg");
-	int64_t end_time = esp_timer_get_time();
-	int64_t time_elapsed = end_time - start_time;
-	ESP_LOGI(TAG, "display_jpg_file execution time: %lld us\n",
-		 time_elapsed);
-
-#if 0 // TODO
-	const char *ws_msg_lcd_finish = "lcd_finish";
-	httpd_ws_frame_t ws_frame;
-	ws_frame.type = HTTPD_WS_TYPE_TEXT;
-	ws_frame.payload = (uint8_t *)ws_msg_lcd_finish;
-	ws_frame.len = strlen(ws_msg_lcd_finish);
-	httpd_ws_send_frame(req, &ws_frame);
-#endif
-
-	return ESP_OK;
-}
+esp_err_t upload_post_handler(httpd_req_t *req);
 
 httpd_uri_t upload_uri = { .uri = "/upload",
 			   .method = HTTP_POST,
@@ -460,6 +327,144 @@ void start_http_server()
 		httpd_register_uri_handler(server, &ws);
 	}
 }
+
+esp_err_t upload_post_handler(httpd_req_t *req)
+{
+	char buf[BUFFER_SIZE];
+	int received;
+	size_t remaining_size = MAX_FILE_SIZE;
+
+	ESP_LOGI(TAG, "httpd_req_t *req->method= %d", req->method);
+	ESP_LOGI(TAG, "httpd_req_t *req.uri= %s", req->uri);
+	ESP_LOGI(TAG, "httpd_req_t *req.content_len= %d", req->content_len);
+
+	// 初始化 PSRAM 缓存
+	psram_buffer_t *psram_buf = init_psram_buffer(MAX_FILE_SIZE);
+	if (!psram_buf) {
+		return ESP_FAIL;
+	}
+
+	ESP_LOGI(TAG, "Receiving file and storing to PSRAM...");
+
+	// 循环接收并存储到 PSRAM
+	while ((received = httpd_req_recv(req, buf, sizeof(buf))) > 0) {
+		// 检查剩余空间是否足够
+		if (psram_buf->offset + received > psram_buf->size) {
+			ESP_LOGE(TAG,
+				 "Not enough PSRAM space to store the file");
+			free_psram_buffer(psram_buf);
+			return ESP_FAIL;
+		}
+
+		// 将接收到的数据拷贝到 PSRAM 中
+		memcpy(psram_buf->data + psram_buf->offset, buf, received);
+		psram_buf->offset += received;
+		remaining_size -= received;
+
+		printf(".");
+		fflush(stdout); // 手动刷新缓冲区
+	}
+	printf("\r\n");
+
+	if (received < 0) {
+		ESP_LOGE(TAG, "File upload failed");
+		free_psram_buffer(psram_buf);
+		return ESP_FAIL;
+	}
+
+	// find file start
+	const unsigned char *start_string = (unsigned char *)"\x0d\x0a\x0d\x0a";
+	const void *pos = memmem(psram_buf->data, 1024, start_string, 4);
+	if (pos == NULL) {
+		ESP_LOGE(TAG, "File received has fault");
+	}
+	ptrdiff_t offset_file_start = (const unsigned char *)pos -
+				      (const unsigned char *)(psram_buf->data);
+	offset_file_start += 4;
+	ESP_LOGI(TAG, "File offset = %d", (int)offset_file_start);
+
+	// find file ends
+	const char *end_string = "------WebKitFormBoundary";
+	size_t search_size = req->content_len > 1024 ?
+				     1024 :
+				     req->content_len; // Adjust search size
+
+	pos = memmem(psram_buf->data + req->content_len - search_size,
+		     search_size, end_string, strlen(end_string));
+	ptrdiff_t offset_file_end = (const unsigned char *)pos -
+				    (const unsigned char *)(psram_buf->data);
+	offset_file_end -= 2;
+	ESP_LOGI(TAG, "File offset = %d", (int)offset_file_end);
+
+	if ((offset_file_end < offset_file_start) || (offset_file_start < 0)) {
+		ESP_LOGI(TAG, "File offset file fail");
+		// Send success response in JSON format
+		httpd_resp_set_type(req, "application/json");
+		const char *resp_str =
+			"{\"code\":500, \"msg\":\"Upload data parse fail.\"}";
+		httpd_resp_send(req, resp_str, strlen(resp_str));
+
+		return ESP_FAIL;
+	}
+
+	ESP_LOGI(TAG, "File upload successful, total size: %zu bytes",
+		 psram_buf->offset);
+
+	// Send success response in JSON format
+	httpd_resp_set_type(req, "application/json");
+	const char *resp_str = "{\"code\":200, \"msg\":\"Upload complete.\"}";
+	httpd_resp_send(req, resp_str, strlen(resp_str));
+
+	// 创建并打开文件
+	FILE *f = fopen(SDCARD_MOUNT_POINT "/request.bin", "wb+");
+	if (f == NULL) {
+		ESP_LOGE("SDMMC", "Failed to open file for writing");
+		sdcard_unmount();
+		return ESP_FAIL;
+	}
+
+	// 写入内容到文件
+	fwrite(psram_buf->data, sizeof(char), psram_buf->offset, f);
+	fclose(f);
+
+	f = fopen(SDCARD_MOUNT_POINT "/upload.jpg", "wb+");
+	if (f == NULL) {
+		ESP_LOGE("SDMMC", "Failed to open file for writing");
+		sdcard_unmount();
+		return ESP_FAIL;
+	}
+
+	// 写入内容到文件
+	fwrite(psram_buf->data + offset_file_start, sizeof(char),
+	       offset_file_end - offset_file_start, f);
+	fclose(f);
+
+	// TODO: 进度条
+
+	// 释放 PSRAM 缓存
+	free_psram_buffer(psram_buf);
+
+	int64_t start_time = esp_timer_get_time();
+	display_jpg_file(SDCARD_MOUNT_POINT "/upload.jpg");
+	int64_t end_time = esp_timer_get_time();
+	int64_t time_elapsed = end_time - start_time;
+	ESP_LOGI(TAG, "display_jpg_file execution time: %lld us\n",
+		 time_elapsed);
+
+	trigger_async_send(req->handle, req);
+
+#if 0 // TODO
+	const char *ws_msg_lcd_finish = "lcd_finish";
+	httpd_ws_frame_t ws_frame;
+	ws_frame.type = HTTPD_WS_TYPE_TEXT;
+	ws_frame.payload = (uint8_t *)ws_msg_lcd_finish;
+	ws_frame.len = strlen(ws_msg_lcd_finish);
+	httpd_ws_send_frame(req, &ws_frame);
+#endif
+
+	return ESP_OK;
+}
+
 
 // 定时器回调函数：释放缓存
 void cache_timer_callback(void *arg)
