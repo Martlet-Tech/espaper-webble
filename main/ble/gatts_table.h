4d23e1eda955690b38a7bd5ca70bb7d52648dfa9
ghosoft <zhaitao.as@outlook.com>
2026-01-14 10:08:23 +0800

    add ble example

---
+++ main/ble/gatts_table.h
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#ifndef __GATTS_TABLE_H__
+#define __GATTS_TABLE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* Attributes State Machine */
+enum
+{
+    IDX_SVC,
+    IDX_CHAR_A,
+    IDX_CHAR_VAL_A,
+    IDX_CHAR_CFG_A,
+
+    IDX_CHAR_B,
+    IDX_CHAR_VAL_B,
+
+    IDX_CHAR_C,
+    IDX_CHAR_VAL_C,
+
+    HRS_IDX_NB,
+};
+
+void gatts_main(void);
+
+#endif
